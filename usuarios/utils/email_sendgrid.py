101a3121eb5ccaa1215f8b2d6563d509d1229f4e
STTNZM247 <you@example.com>
2025-12-10 12:30:15 -0500

    configuracion de setting14

--- usuarios/utils/email_sendgrid.py
+++ usuarios/utils/email_sendgrid.py
@@ -2,6 +2,7 @@ import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 from django.conf import settings
+from usuarios.models import EmailLog
 
 def send_email_sendgrid(to_email, subject, html_content):
     message = Mail(
@@ -10,9 +11,21 @@ def send_email_sendgrid(to_email, subject, html_content):
         subject=subject,
         html_content=html_content
     )
+    exito = False
+    error = ''
     try:
         sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
         response = sg.send(message)
+        exito = response.status_code == 202
         return response.status_code
     except Exception as e:
-        return str(e)
+        error = str(e)
+        return error
+    finally:
+        EmailLog.objects.create(
+            destinatario=to_email,
+            asunto=subject,
+            contenido=html_content,
+            exito=exito,
+            error=error
+        )
